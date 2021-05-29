93c614d4ec57c1705c5c5e426702fde51b582a62
subhanprime <subhan25902@gmail.com>
2021-05-29 10:28:30 +0500

    backend setup

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,18 @@
-console.log('backend')
+const express = require('express')
+const cors = require('cors')
+require ('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5001;
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/' , (req ,res)=>{
+    res.send('<h1>ho i am laundry app</h1>');
+    console.log('laundry app home page')
+})
+
+app.listen(port , ()=>{
+    console.log(`server is running on port ${port}`) 
+});
